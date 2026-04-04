fb267b4c4801f8711958fe59804e098c0b8b2941
Matthew Keeler <mkeeler@launchdarkly.com>
2026-04-04 10:10:44 -0400

    fix: Update copy alias for wayland support

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -6,7 +6,7 @@ fi
 
 alias -g @j='| jq "."'
 alias -g @r='| vim -R'
-alias -g @x='| xclip -i -selection clipboard'
+alias -g @x='| wl-copy'
 alias -g @sum='| awk "{s+=\$1} END {print s}"'
 alias -g @avg='| awk "{s+=\$1} END {print s/NR}"'
 
