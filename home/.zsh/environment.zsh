59d6b6a8495c8fbe8a269c3865a4d72af0d972bc
Matthew Keeler <mkeeler@launchdarkly.com>
2026-05-15 18:26:34 -0400

    chore: Set CLAUDE_CONFIG_DIR

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -13,6 +13,8 @@ export BAT_THEME="Solarized (dark)"
 export TERM=${TERM:-xterm-256color}
 export LANG=en_US.UTF-8
 
+export CLAUDE_CONFIG_DIR="$HOME/.claude"
+
 export FZF_DEFAULT_OPTS='
     --color fg:223,bg:-1,hl:65,fg+:142,bg+:-1,hl+:108
     --color info:108,prompt:109,spinner:108,pointer:168,marker:168
